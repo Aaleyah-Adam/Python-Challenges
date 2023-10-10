cbf68a2c71406cfce270e5df75cbb710374f0c4f
Aaleyah-Adam <117378959+Aaleyah-Adam@users.noreply.github.com>
2023-10-10 14:07:09 +0800

    Create TU03.py

---
+++ TU03.py
@@ -0,0 +1,8 @@
+#Exercise 1
+MyAge = int(input('What is your age?'))
+print('%d is a very nice age to be' % MyAge)
+
+#Exercise 2 
+ComputerAge=20.456
+ComputerName='Skynet'
+print('I am younger than %d, because I am %.2f My name is %s.' % (MyAge,ComputerAge,ComputerName))
